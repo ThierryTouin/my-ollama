700bc7b584b7c8bf7ca023bb75faf3d44cf034ec
thierry <thierrytouin@gmail.com>
2025-10-13 17:24:38 +0200

    new test

--- cmd.sh
+++ cmd.sh
@@ -7,9 +7,10 @@ function display_help {
   echo "  cmd COMMAND [options] [arguments]"
   echo
   echo "front Commands:"
-  echo "  cmd up          Start the application"
-  echo "  cmd shell       Enter in container"
-  echo "  cmd shellr      Enter in container with root user"
+  echo "  cmd up..........Start the application"
+  echo "  cmd down........Stop the application"
+  echo "  cmd shell.......Enter in container"
+  echo "  cmd shellr......Enter in container with root user"
   echo "  cmd clean.......Clean the application"
   echo
   exit 1
@@ -20,6 +21,8 @@ if [ $# -gt 0 ]; then
     docker compose exec -it ollama bash
   elif [ "$1" == "shellr" ]; then
     docker container exec -it --user root vm-ssl-tester-app1 /bin/sh
+  elif [ "$1" == "down" ]; then
+    docker compose down 
   elif [ "$1" == "clean" ]; then
     docker compose down --volumes --rmi all
   elif [ "$1" == "help" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
